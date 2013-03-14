9fe89d9761f6a1b2070cb3e05a0f063dcb596eac
Fjodor Ekström <fjodor.ekstrom@avail.net>
2013-03-14 14:15:59 +0100

    simple maven junit project

---
+++ src/main/java/Multiplier.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: fjodorekstrom
+ * Date: 3/14/13
+ * Time: 2:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Multiplier {
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+}
---
+++ src/test/java/MultiplierTests.java
@@ -0,0 +1,19 @@
+import com.sun.corba.se.impl.orb.ParserTable;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fjodorekstrom
+ * Date: 3/14/13
+ * Time: 2:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MultiplierTests {
+    @Test
+    public void test2x2Is4() {
+        Multiplier multiplier = new Multiplier();
+        assertTrue(multiplier.multiply(2, 2) == 4);
+    }
+}
